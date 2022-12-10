334bbcee708fe2741b5f1fa92c34aeeef832163c
kyousuke916 <kyousuke916@gmail.com>
2022-12-10 17:04:55 +0800

    test

---
+++ Assets/Runtime/MyComponen2.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MyComponen2 : MonoBehaviour
+{
+    public int Value;
+
+    void Start()
+    {
+        Debug.Log($"MyComponen2 Value:{Value}");
+    }
+}
---
+++ Assets/Runtime/MyFunc2.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Test2
+{
+    public class MyFunc2 : MonoBehaviour
+    {
+        public int Value = 0;
+
+        public void SetValue(int value)
+        {
+            Value = value;
+        }
+
+        public void Execute()
+        {
+            Debug.Log($"MyFunc2.Execute Value:{Value}");
+        }
+    }
+}
